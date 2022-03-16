aacded771a628535e2b2668c4779f2f1845017c0
Jinho <breezjin@gmail.com>
2022-03-16 11:33:10 +0900

    fix subpages layout

--- src/features/errorPage/ErrorPage.js
+++ src/features/errorPage/ErrorPage.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function ErrorPage() {
   return (
     <StyledErrorPage>
       <div>ErrorPage</div>
+      <br />
+      <Link to='/' className='redirection-link'>
+        메인으로 돌아가기
+      </Link>
     </StyledErrorPage>
   );
 }
 
 const StyledErrorPage = styled.div`
-  z-index: 20;
-  width: 90%;
-  height: 90%;
+  z-index: 5;
+  width: calc(100% - 2rem);
+  height: calc(100% - 2rem);
   background-color: #000000a2;
   border-radius: 0.3rem;
+  margin: 1rem 0rem 0rem 0rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
